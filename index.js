63e1eb7e9b8ab5c0c473f83dd455d2b34eed3923
ChaseTeague <teaguechase@yahoo.com>
2023-12-03 04:19:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+//declare function
+function hasTargetSum(array, target){
+//create for loop to iterate over array
+  for(let i = 0; i < array.length; i++){
+    //declare a variable to store a compliment number to the target
+    let num = target - array[i];
+    //create a for loop to iterate to continue over the rest of the numbers
+    for(let j = i + 1; j < array.length; j++){
+      //create and if statement that returns true or false if target sum is or isn't found in array
+      if(array[j] === num) return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime = O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here 
+  declare function
+  create for loop to iterate over array
+  declare a variable to store a compliment number to the target
+  create a for loop to iterate to continue over the rest of the numbers
+  create and if statement that returns true or false if target sum is or isn't found in array
 */
 
 /*
   Add written explanation of your solution here
+  creating a function called hasTargetSum that will take in 2 arguements one being an array and one being a target 
+  number. The function will take in an array and begin to iterate over the numbers in said array
+  until it finds 2 numbers which sum equals the target.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +49,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 4, 9, 17], 12));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([140, 101, 225, 78], 365));
+
+
 }
 
 module.exports = hasTargetSum;
